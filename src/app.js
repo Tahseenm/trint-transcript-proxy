d27f79a5fea60ba9e39ced7145bb6ae29b1adc2e
Tahseen Malik <thas_01@live.com>
2018-02-24 09:49:51 +0000

    feat: Add error handling

--- src/app.js
+++ src/app.js
@@ -9,5 +9,24 @@ app.get('/', (request, respond) => {
   })
 })
 
+/** Catch 404 */
+app.use((request, respond, next) => {
+  const error  = new Error('Not Found')
+  error.status = 404
+  next(error)
+})
+
+/** Handle all request errors */
+app.use((error, request, respond, next) => {
+  if (request.headersSent) next(error)
+
+  respond.status(error.status || 500)
+  respond.json({
+    errors: {
+      message: error.message,
+    },
+  })
+})
+
 
 export default app
