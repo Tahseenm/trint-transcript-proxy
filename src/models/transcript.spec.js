75f301ea3dfea6ee628c6653abe0eb8b9787b3c4
Tahseen Malik <thas_01@live.com>
2018-02-25 21:10:39 +0000

    refactor: Create fixtures dir

--- src/models/transcript.spec.js
+++ src/models/transcript.spec.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import MockAdapter from 'axios-mock-adapter'
 import getTranscript, { TRANSCRIPT_S3_URL } from './transcript'
 
-import transcript from './transcript.fixture.json'
+import transcript from './fixtures/transcript.json'
 import { isPromise } from '../utils'
 
 
