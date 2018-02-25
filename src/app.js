43c554b75f65cbd31b55306ff191a90dd3f39042
Tahseen Malik <thas_01@live.com>
2018-02-25 17:06:21 +0000

    feat: Print stacktrace during development

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import routes  from './routes'
+import { isDevelopment } from './utils'
 
 
 const app = express()
@@ -8,24 +9,32 @@ const app = express()
 /** API routes */
 app.use('/api/v1', routes)
 
-/** Catch 404 */
+/** Catch 404's! */
 app.use((request, respond, next) => {
   const error  = new Error('Not Found')
   error.status = 404
   next(error)
 })
 
-/** Handle all request errors */
-app.use((error, request, respond, next) => {
-  if (request.headersSent) next(error)
+
+/**
+ Handle all request errors!
+ [1]. Print & send stacktrace only in development.
+ */
+
+/* eslint-disable no-console */
+app.use((error, request, respond) => {
+  if (isDevelopment()) console.log(error)   /* [1] */
 
   respond.status(error.status || 500)
   respond.json({
     errors: {
       message: error.message,
+      error: isDevelopment() ? error : {},  /* [1] */
     },
   })
 })
+/* eslint-enable no-console */
 
 
 export default app
