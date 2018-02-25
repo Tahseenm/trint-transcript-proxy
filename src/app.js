08814ef878cec99fbaefc2eaecbbfb720ac9ea85
Tahseen Malik <thas_01@live.com>
2018-02-25 17:36:22 +0000

    feat: Add cross origin resource sharing support

--- src/app.js
+++ src/app.js
@@ -1,10 +1,21 @@
 import express from 'express'
+import cors from 'cors'
+import errorHandler from 'errorhandler'
+
 import routes  from './routes'
 import { isDevelopment } from './utils'
 
 
 const app = express()
 
+
+/**
+ Global express middlewares
+ [1]. Add Cross origin resource sharing support.
+ [2]. Add express error handler for development.
+ */
+app.use(cors())                               /* [1] */
+if (isDevelopment()) app.use(errorHandler())  /* [2] */
 
 /** API routes */
 app.use('/api/v1', routes)
@@ -22,19 +33,19 @@ app.use((request, respond, next) => {
  [1]. Print & send stacktrace only in development.
  */
 
-/* eslint-disable no-console */
-app.use((error, request, respond) => {
-  if (isDevelopment()) console.log(error)   /* [1] */
+/* eslint-disable no-console, no-unused-vars */
+app.use((error, request, respond, next) => {
+  if (isDevelopment()) console.error(error)  /* [1] */
 
   respond.status(error.status || 500)
   respond.json({
     errors: {
       message: error.message,
-      error: isDevelopment() ? error : {},  /* [1] */
+      error: isDevelopment() ? error : {},   /* [1] */
     },
   })
 })
-/* eslint-enable no-console */
+/* eslint-enable no-console, no-unused-vars */
 
 
 export default app
